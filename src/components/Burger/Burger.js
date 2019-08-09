22d630c713758265465dc727c555ae8808453c86
Tom Lam <tomlam0828@gmail.com>
2019-08-09 00:09:59 -0700

    new-update2

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import { withRouter } from 'react-router-dom'
 
 const burger = (props) => {
+    console.log(props)
     let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />
@@ -24,4 +26,4 @@ const burger = (props) => {
     );
 }
 
-export default burger;
+export default withRouter(burger);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,6 +26,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-a2a83.firebaseio.com/ingredients.json').then(response => {
             this.setState({ ingredients: response.data })
         })
@@ -83,26 +84,15 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // alert('You continue!')
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice.toFixed(2),
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'test',
-                    zipCode: '98122',
-                    country: 'The USA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order).then(response => {
-            this.setState({ loading: false, purchasing: false })
-        }).catch(error => {
-            this.setState({ loading: false, purchasing: false })
+        queryParams.push('price=' + this.state.totalPrice)
+        const queryString = queryParams.join('&')
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         })
     }
 
