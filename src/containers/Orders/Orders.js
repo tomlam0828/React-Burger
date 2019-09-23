a3a999ca4032c112ce488f9afd0babefe77d416a
Tom Lam <tomlam0828@gmail.com>
2019-09-23 16:19:17 -0700

    update

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,44 +1,47 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 class Orders extends Component {
 
-    state = {
-        orders: [],
-        loading: true
-    }
-
     componentDidMount() {
-        axios.get('/orders.json').then(res => {
-            const fetchOrders = [];
-            for (let key in res.data) {
-                fetchOrders.push({
-                    ...res.data[key],
-                    id: key
-                });
-            }
-            this.setState({ loading: false, orders: fetchOrders });
-        }).catch(err => {
-            this.setState({ loading: false });
-        })
+        this.props.onFetchOrders()
     }
 
     render() {
+        let orders = <Spinner />;
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                <Order
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}
+                />
+            ));
+        };
         return (
             <div>
-                {this.state.orders.map(order => (
-                    <Order
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={order.price}
-                    />
-                ))}
+                {orders}
             </div>
-
         );
     }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToPtops = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(mapStateToPtops, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,41 +1,60 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     orders: [],
     loading: false,
     purchased: false
+};
+
+const purchaseInit = (state, action) => {
+    return updateObject(state, { purchased: false });
+};
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, { loading: true });
+
+};
+
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, { id: action.orderIf });
+    return updateObject(state, {
+        loading: false,
+        purchased: true,
+        orders: state.orders.concat(newOrder)
+    });
+};
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, { loading: false });
+
+};
+
+const fetchOrdersStart = (state, action) => {
+    return updateObject(state, { loading: true });
+};
+
+const fetchOrderSuccess = (state, action) => {
+    return updateObject(state, {
+        orders: action.orders,
+        loading: false
+    });
+};
+
+const fetchOrderFail = (state, action) => {
+    return updateObject(state, { loading: false });
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHSE_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
-        case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderDate,
-                id: action.orderId
-            }
-            return {
-                ...state,
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(newOrder)
-            }
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                loading: false
-            }
-        default:
-            return state;
+        case actionTypes.PURCHSE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrderSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderFail(state, action);
+        default: return state;
     }
 }
 
