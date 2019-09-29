5645b74bec1fd358e9537b0f4662e234ddd77fcd
Tom Lam <tomlam0828@gmail.com>
2019-09-29 16:38:21 -0700

    new update

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import createSagaMiddleware from 'redux-saga';
+import {watchAuth} from './store/sagas'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
@@ -17,11 +19,15 @@ const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
-})
+});
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
