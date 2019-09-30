ef1017038773ce8932e5cff4e0662a7ce763a362
Tom Lam <tomlam0828@gmail.com>
2019-09-30 10:43:11 -0700

    new update

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,41 +30,28 @@ export const logout = () => {
     return {
         type: actionTypes.AUTH_INITIATE_LOGOUT
     }
+};
+
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
 }
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout())
-        }, expirationTime * 1000)
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
     }
 }
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCP8gyBi05FpOGiugGNy-mu72asDOxcA6M';
-        if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCP8gyBi05FpOGiugGNy-mu72asDOxcA6M'
-        }
-        axios.post(url, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn))
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error))
-            })
-    };
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
+    }
 };
 
 export const setAuthRedirectPath = (path) => {
